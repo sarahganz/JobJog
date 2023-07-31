ddd9f1d104478eed6510a27425f7f145c6ec8778
Sarah Ganz <strolisarah@gmail.com>
2023-07-31 17:08:15 -0400

    basic setup and installations for aws apis and authentication

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+import os
+import uuid
+import boto3
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
