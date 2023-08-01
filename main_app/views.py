52011c94a028e415620aab740bdae7293739dac9
Sarah Ganz <135158251+sarahganz@users.noreply.github.com>
2023-08-01 15:17:23 -0400

    Update views.py

--- main_app/views.py
+++ main_app/views.py
@@ -10,5 +10,3 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
